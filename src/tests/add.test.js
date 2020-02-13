4162a9a1a82d0df5dad958fadc50b93f7d03969b
Alejo <alejoboga20@gmail.com>
2020-02-13 16:28:43 -0300

    Test for Expense Action

--- src/tests/add.test.js
+++
@@ -1,6 +0,0 @@
-const generateGreeting = (name) => `Hello ${name}`;
-
-test('Should return a greeting', ()=> {
-    const result = generateGreeting('world');
-    expect(result).toBe('Hello world');
-});
